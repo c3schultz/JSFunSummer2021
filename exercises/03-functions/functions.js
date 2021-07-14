c0c89748f7155a30acd52458179891a3421d68fd
c3schultz <c3schultz@gmail.com>
2021-07-14 16:59:12 -0400

    HW 3

--- exercises/03-functions/functions.js
+++ exercises/03-functions/functions.js
@@ -8,13 +8,23 @@
  * @example printGreeting("Tim"); // Hello Tim!
  */
 
+const printGreeting = (firstName) => {
+  console.log("Hello", firstName + '!');
+  return "Hello ${firstName} + !";
+  };
+  printGreeting("Cam");
 /**
  * Create a function named "timesFive". It should multiply a number by 5.
  * @example timesFive(5); // 25
  * @example timesFive(2); // 10
  * @example timesFive(0); // 0
  */
-
+const timesFive = (num) => {
+  return 5 * num;
+};
+  timesFive(5);
+  timesFive(2);
+  timesFive(0);
 /**
  * Create a function called "lowerCaseName".
  * If given a string, it should return the string lower cased.
@@ -24,7 +34,19 @@
  * @example lowerCaseName(); // undefined (should not throw an error)
  * @example lowerCaseName(null); // undefined (should not throw an error)
  */
+const lowerCaseName = (string) => {
+  //console.log('HELLO WORLD'.toLowerCase());
+  const notAString = (false);
 
+  if (lowerCaseName); {
+   
+    if (notAString) console.log("loading...");
+    else console.log('HELLO WORLD'.toLowerCase());
+  }
+   return string; 
+};
+  lowerCaseName();
+  lowerCaseName(null);
 /**
  * Create a function called "isEvenOrOdd".
  * If given an even number, it should return "even".
@@ -32,13 +54,30 @@
  * @example isEvenOrOdd(10); // even
  * @example isEvenOrOdd(3); // odd
  */
+const isEvenOrOdd = (num) => {
+  let isEven = "even";
+  let isOdd = "odd";
+if (num % 2 == 1) {
+  return isOdd; 
+}
+  return isEven;
+};
 
+  isEvenOrOdd(10);
+  isEvenOrOdd(3);
 /**
  * Create a function called "countNumberOfDigits".
  * Given an integer, it should return the number of digits in an integer.
  * @example countNumberOfDigits(5000); // 4
  */
-
+const countNumberOfDigits = (num) => {
+  let string = num;
+  string = "5000";
+  //console.log(string.length);
+  return string.length;
+};
+  countNumberOfDigits("5000");
+  //countNumberOfDigits();
 /**
  * Ignore everything below this line. This is for the tests.
  */
